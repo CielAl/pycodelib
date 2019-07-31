7de9a99945d3aa1da80e6ca5f83ca0996dc80d0d
Ciel <cielmercy@gmail.com>
2019-07-31 02:59:34 -0400

    reformat the H5SetBasic and the IteratorGetters

--- pycodelib/dlengine/iterator_getter.py
+++ pycodelib/dlengine/iterator_getter.py
@@ -1,41 +1,38 @@
-from typing import Dict
-
+from typing import Dict, Callable
 from torch.utils.data import DataLoader
 # from torch.utils.data import Dataset
-from pycodelib.dataset import H5SetTransform as H5Set
-from pycodelib.dlengine.skeletal import IteratorBuilder
+from pycodelib.dataset import H5SetBasic
+from pycodelib.dlengine.skeletal import AbstractIteratorBuilder
 
 
-class H5DataGetter(IteratorBuilder):
-    _datasets_container: Dict[bool, IteratorBuilder] = None
+class H5DataGetter(AbstractIteratorBuilder):
     BATCH_SIZE: int = 32
 
-    def __init__(self, filename: str, img_transform, mode, constructor: type = H5SetTransform):
-        dataset: constructor = constructor(filename, img_transform=img_transform)
-        super().__init__(dataset)
-        self._train_mode = mode
-
-
-    def build(self, filename_dict: Dict[bool, str], img_transform_dict: Dict[bool, object]):
-        assert filename_dict.keys() == img_transform_dict.keys(), 'Keys disagree.'
-        cls._datasets_container: Dict[bool, cls] = {x: H5DataGetter(filename_dict[x], img_transform_dict[x], x)
-                                                    for x in filename_dict.keys()}
-        return cls
-
-    @property
-    def mode(self):
-        return self._train_mode
-
-    @property
-    def dataset(self):
-        return super()._dataset
+    def __init__(self, datasets: Dict[bool, H5SetBasic], img_transform_dict: Dict[bool, Callable]):
+        self._datasets_collection: Dict[bool, H5SetBasic] = datasets
+        self._transform: Dict[bool, Callable] = img_transform_dict
+        self._mode: bool = True
 
     @classmethod
-    def get_iterator(cls, mode: bool, shuffle: bool,
+    def build(cls, filename_dict: Dict[bool, str], img_transform_dict: Dict[bool, Callable]):
+        assert filename_dict.keys() == img_transform_dict.keys(), 'Keys disagree.'
+        datasets: Dict[bool, H5SetBasic] = {
+                    x: H5SetBasic(filename_dict[x])
+                    for x in filename_dict.keys()
+        }
+        iter_getter = cls(datasets=datasets, img_transform_dict=img_transform_dict)
+        return iter_getter
+
+    @property
+    def datasets_collection(self):
+        return self._datasets_collection
+
+    def get_iterator(self, mode: bool, shuffle: bool = None,
                      num_workers: int = 6,
                      drop_last: bool = False,
                      pin_memory: bool = True,
                      batch_size: int = BATCH_SIZE):
-        assert mode == cls._datasets_container[mode].mode, f"Mode mismatch. input:{mode}"
-        return DataLoader(cls._datasets_container[mode].dataset, num_workers=num_workers, pin_memory=pin_memory,
-                          batch_size=batch_size, shuffle=mode, drop_last=True)
+        if shuffle is None:
+            shuffle = mode
+        return DataLoader(self.datasets_collection[mode], num_workers=num_workers, pin_memory=pin_memory,
+                          batch_size=batch_size, shuffle=shuffle, drop_last=True)
