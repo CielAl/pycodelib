97b24aec5e9bf6df87f1a41b57153a2696c47142
Ciel <cielmercy@gmail.com>
2019-08-06 01:12:45 -0400

    Commented

--- pycodelib/dlengine/base_engine.py
+++ pycodelib/dlengine/base_engine.py
@@ -1,4 +1,7 @@
-from .skeletal import AbstractEngine, ModeIteratorBuilder
+"""
+    The basic implementation of engines.
+"""
+from .skeletal import AbstractEngine, AbstractIteratorBuilder
 from .multi_instance_meter import MultiInstanceMeter
 from pycodelib.patients import SheetCollection
 from typing import Tuple, Dict, Any, Sequence, List, Callable
@@ -7,29 +10,45 @@ import torch.nn as nn
 from torchnet.meter import AverageValueMeter, ClassErrorMeter, ConfusionMeter, AUCMeter
 from tqdm import tqdm
 import logging
+
 logging.basicConfig(level=logging.debug)
+softmax: Callable = nn.Softmax(dim=1)
 
 
 class SkinEngine(AbstractEngine):
-
-    def __init__(self, device: torch.device, model: nn.Module, loss: nn.Module, iterator_getter: ModeIteratorBuilder,
+    """
+        Engine for the skin cancer project. Patient-level.
+    """
+    def __init__(self, device: torch.device,
+                 model: nn.Module,
+                 loss: nn.Module,
+                 iterator_getter: AbstractIteratorBuilder,
                  val_phases: Sequence[str, ...],
-                 class_names: int,
+                 class_names: List,
                  patient_col: SheetCollection):
         super().__init__(device, model, loss, iterator_getter)
         self.val_phases = val_phases
         self.class_names = class_names
         self.num_classes = len(class_names)
-        self.membership = None  # todo
+        self.membership = None  # todo - EM
         self.patient_col = patient_col
 
-        self.add_meter('accuracy_meter', ClassErrorMeter(accuracy=True))
+        self.add_meter('patch_accuracy_meter', ClassErrorMeter(accuracy=True))
         self.add_meter('conf_meter', ConfusionMeter(self.num_classes, normalized=False))
         self.add_meter('loss_meter', AverageValueMeter())
         self.add_meter('multi_instance_meter', MultiInstanceMeter(self.patient_col))
         self.add_meter('auc_meter', AUCMeter())
 
     def model_eval(self, data_batch: List) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+
+        Args:
+            data_batch: List[batches_var1, batches_var2, ...]. Each list element is a batch of data variable
+             (i.e. [batch_img, batch_label])
+        Returns:
+            loss: the loss
+            prediction: the prediction of the output layer
+        """
         img, label, *rest = data_batch
         img = img.to(self.device)
         label = label.type('torch.LongTensor').to(self.device)
@@ -38,25 +57,75 @@ class SkinEngine(AbstractEngine):
         return loss, prediction
 
     def on_start(self, state: Dict[str, Any]):
-        logging.debug(f"Start...")
+        """
+            Initiation of procedure
+        Args:
+            state:
+
+        Returns:
+
+        """
+        logging.info(f"Start...")
 
     def on_end(self, state: Dict[str, Any]):
+        """
+        End of procedure
+        Args:
+            state: The dict storage to pass variables.
+
+        Returns:
+            None
+        """
         torch.cuda.empty_cache()
         logging.debug(f"End...")
 
     def on_sample(self, state: Dict[str, Any]):
+        """
+
+        Args:
+            state:
+
+        Returns:
+
+        """
         state['sample'].append(state['train'])
         self.model.train(state['train'])
 
     def on_update(self, state: Dict[str, Any]):
+        """
+            Pass
+        Args:
+            state:
+
+        Returns:
+
+        """
         ...
 
     def on_start_epoch(self, state: Dict[str, Any]):
+        """
+            Reset all meters and mark mode as training.
+            Also initialize the tqdm progress bar
+        Args:
+            state:
+
+        Returns:
+
+        """
         self._reset_meters()
         state['train'] = True
         state['iterator'] = tqdm(state['iterator'])
 
     def on_end_epoch(self, state: Dict[str, Any]):
+        """
+            Initialize the test procedure to evaluate the model performance.
+            Mark mode as False.
+        Args:
+            state:
+
+        Returns:
+
+        """
         state['train'] = False
         self.engine.test(self, self.iterator_getter.get_iterator(mode=False, shuffle=False))
 
@@ -68,11 +137,16 @@ class SkinEngine(AbstractEngine):
         # todo test
         ...
         print('[Epoch %d] Testing Loss: %.4f (Accuracy: %.2f%%)' % (
-            state['epoch'], self.meter_dict['loss_meter'].value()[0], self.meter_dict['accuracy_meter'].value()[0]))
+            state['epoch'], self.meter_dict['loss_meter'].value()[0], self.meter_dict['patch_accuracy_meter'].value()[0]))
         print(self.meter_dict['conf_meter'].value())
         self._fetch_prediction()
 
     def _fetch_prediction(self):
+        """
+            -todo Consider move to the multi_instance_meter
+        Returns:
+
+        """
         patch_score = self.meter_dict['multi_instance_meter'].value()
         patch_label_collection = {
                         key: [score_array.argmax(dim=1) for score_array in score_array_list]
@@ -87,22 +161,40 @@ class SkinEngine(AbstractEngine):
         filenames = list(patch_label_prediction.keys())
         self.patient_col.load_prediction(pred_labels_str, filenames)
 
-    # measurements
     def on_forward(self, state: Dict[str, Any]):
-        softmax: Callable = nn.Softmax(dim=1)
-        pred_data = state['output'].data
-        loss_scalar = state['loss'].item()
+        """
+            On forward. Perform measurements for both train and test.
+        Args:
+            state:
 
+        Returns:
+
+        """
+
+        # retrieve prediction and loss.
+
+        # pred_data as the output score. Shape: N samples * M classes.
+        pred_data: torch.Tensor = state['output'].data
+        loss_scalar: torch.Tensor = state['loss'].data  # .item().  Unify the behavior to pred_data
+
+        # retrieve input data
         img_new, label, img, filename, *rest = state['sample']
-        label = label.type("torch.LongTensor")
-        self.meter_dict['loss_meter'].add(loss_scalar)
-        self.meter_dict['accuracy_meter'].add(pred_data, label)
+        label: torch.LongTensor = label.type("torch.LongTensor")
 
-        pred_expand: torch.Tensor = pred_data
-        if pred_data.ndimension(pred_data) == 1:
-            pred_expand = pred_data[None]
-        self.meter_dict['conf_meter'].add(pred_expand, label)
-        pred_softmax = softmax(pred_expand)
+        # Add Loss Measurements per batch
+        self.meter_dict['loss_meter'].add(loss_scalar)
+        # Add accuracy (patch) per batch
+        self.meter_dict['patch_accuracy_meter'].add(pred_data, label)
+
+        # If 1-d vector (in case of batch-of-one), adding leading singleton dim.
+        # Otherwise the confusion_meter may mistreat the 1-d vector as a vector of
+        # score of positive classes.
+        if pred_data.ndimension() == 1:
+            pred_data = pred_data[None]
+        self.meter_dict['conf_meter'].add(pred_data, label)
+
+        # Posterior score
+        pred_softmax = softmax(pred_data)
         patch_name_score: Tuple = (filename, pred_softmax)
         self.meter_dict['multi_instance_meter'].add(patch_name_score)
 
--- pycodelib/dlengine/skeletal.py
+++ pycodelib/dlengine/skeletal.py
@@ -1,5 +1,9 @@
+"""
+    Abstract classes.
+"""
 from typing import Callable, Tuple, Dict, Any, List
 import torch
+from torch.utils.data import DataLoader
 from torchnet.engine import Engine
 from abc import ABC, abstractmethod
 from torch.optim.optimizer import Optimizer
@@ -9,98 +13,262 @@ from torchnet.meter.meter import Meter
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-logging.debug(__name__)
 
 class AbstractIteratorBuilder(ABC):
+    """
+        The Encapsulation to assign the DataLoader given the phases/modes (i.e. train/val).
+    """
     BATCH_SIZE: int = 32
     @classmethod
     @abstractmethod
-    def get_iterator(cls, mode, shuffle, num_workers=4, drop_last=False,  pin_memory=True, batch_size=4):
+    def get_iterator(cls, mode, shuffle, num_workers=4, drop_last=False,  pin_memory=True, batch_size=4) -> DataLoader:
+        """
+        Args:
+            mode: True if train, otherwise False.
+            shuffle: True if data are shuffled each epoch.
+            num_workers: Number of CPU.
+            drop_last:  Drop the last batch if smaller than batch_size.
+            pin_memory: Use pin memory (Details in DataLoader of pytorch)
+            batch_size: Size of batch
+
+        Returns:
+            The DataLoader given the mode.
+        """
         ...
 
     @property
     @abstractmethod
     def datasets_collection(self):
+        """
+        Returns:
+            The field which contains all dataset, corresponding to all modes.
+        """
         ...
 
 
 class AbstractEngine(Callable):
+    """
+        The abstraction of training using engine. The class is Callable by itself, where its __call__ is defined to
+        evaluate the model as serve as the "network/model" parameter of tnt/engine
+    """
 
     @property
-    def engine(self):
+    def engine(self) -> Engine:
+        """
+        Returns:
+            The engine property.
+        """
         return self._engine
 
     @property
-    def hooks(self):
+    def hooks(self) -> Dict[str, Callable]:
+        """
+        Returns:
+            The hooks property
+        """
         return self._hooks
 
     @property
-    def model(self):
+    def model(self) -> nn.Module:
+        """
+
+        Returns:
+            The model property. Typically the neural network.
+        """
         return self._model
 
     @property
-    def loss(self):
+    def loss(self) -> nn.Module:
+        """
+
+        Returns:
+            The loss property. The definition of loss append to the network.
+        """
         return self._loss
 
     @abstractmethod
     def model_eval(self, data_batch: List) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+            The helper function that evaluate the model that should be invoked in __call__.
+            It is separated from __call__ to more or less loose the semantic coupling that "__call__", which is
+            a more general procedure, should perform the model evaluation.
+        Args:
+            data_batch: List batch variables. See detail implementations.
+        Returns:
+            loss: Evaluated loss value(s).
+            prediction: Model output.
+        """
         ...
 
     def __call__(self, data_batch: List) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+            Work as the 1st callable parameter of self.engine.train
+        Args:
+            data_batch: Input batch. See model_eval.
+
+        Returns:
+            loss
+            prediction
+        """
         loss, pred = self.model_eval(data_batch)
         return loss, pred
 
-    def __init__(self, device: torch.device, model: nn.Module, loss: nn.Module,
+    def __init__(self,
+                 device: torch.device,
+                 model: nn.Module,
+                 loss: nn.Module,
                  iterator_getter: AbstractIteratorBuilder):
+        """
+
+        Args:
+            device: Device that hosts all variables/models, i.e. the GPU
+            model:  The callable model that is to be trained/evaluated.
+            loss:   The loss that is appended to the output layer.
+            iterator_getter:    The DataLoader assignment by mode.
+        """
+        # Initializing the maxepoch field.
         self._maxepoch: int = -1
+        # The torchnet.engine object.
         self._engine: Engine = Engine()
+        # All possible hooks that is called by engine.train and engine.test
         self._hooks: Dict[str, Callable] = dict({
-                    "on_start": self.on_start,
+                    "on_start": self.on_start,              # start of the procedure
                     "on_start_epoch": self.on_start_epoch,  # train exclusive
                     "on_sample": self.on_sample,            # get data point from the data-loader
                     "on_forward": self.on_forward,          # the only phase that both train/test applies
                     "on_update": self.on_update,            # train exclusive, after the "step" of backward updating
                     "on_end_epoch": self.on_end_epoch,      # train exclusive - usually test is invoked here
-                    "on_end": self.on_end,
+                    "on_end": self.on_end,                  # end of the procedure
         })
+        # The device which host the model and all variables.
         self.device: torch.device = device
+        # The neural network model.
         self._model: nn.Module = model.to(device)
+        # Loss
         self._loss: nn.Module = loss
+        # Iterator getter
         self.iterator_getter: AbstractIteratorBuilder = iterator_getter
+        # Empty Meter storage.
         self.meter_dict: Dict = dict()
 
     def process(self, maxepoch: int, optimizer: Optimizer):
+        """
+            The exposed interface to user, which is an encapsulation of engine.train, where the 1st callable
+             parameter of engine.train is self (i.e. defined by __call__)
+        Args:
+            maxepoch:
+            optimizer:
+
+        Returns:
+
+        """
         self._maxepoch = maxepoch
         self.engine.train(self, self.iterator_getter.get_iterator(mode=True, shuffle=True),
                           maxepoch=self._maxepoch, optimizer=optimizer)
 
     @abstractmethod
     def on_start(self, state: Dict[str, Any]):
+        """
+            Hook that is executed on initiation.
+        Args:
+            state: A dict storage to pass the variables with initial value:
+                state = {
+                    'network': network,
+                    'iterator': iterator,
+                    'maxepoch': maxepoch,
+                    'optimizer': optimizer,
+                    'epoch': 0,
+                    't': 0,
+                    'train': True,
+                }
+        Returns:
+
+        """
         ...
 
     @abstractmethod
     def on_start_epoch(self, state: Dict[str, Any]):
+        """
+            Hook that is invoked on each single epoch. Typically reset the meters or setting the modes
+            of dataset (train or test).
+            Only available in the training mode.
+        Args:
+            state: See on_start
+
+        Returns:
+
+        """
         ...
 
     @abstractmethod
     def on_sample(self, state: Dict[str, Any]):
+        """
+            Hook that is invoked immediately after a sample (possibly a batch) is extracted from the DataLoader.
+        Args:
+            state: See on_start
+
+        Returns:
+
+        """
         ...
 
     @abstractmethod
     def on_forward(self, state: Dict[str, Any]):
+        """
+            Hook that is invoked after the forwarding (execution of model) and loss.backward, and before on_update.
+        Args:
+            state: See on_start
+
+        Returns:
+
+        """
         ...
 
     @abstractmethod
     def on_update(self, state: Dict[str, Any]):
+        """
+            Hook that is invoked after the on_forward, i.e. after the back-propagation and optimization.
+            Only available in training mode.
+        Args:
+            state: See on_start
+
+        Returns:
+
+        """
         ...
 
     @abstractmethod
     def on_end_epoch(self, state: Dict[str, Any]):
+        """
+            Hook that is invoked on the termination of each epoch.
+            Only available in training mode.
+        Args:
+            state:
+
+        Returns:
+
+        """
         ...
 
     @abstractmethod
     def on_end(self, state: Dict[str, Any]):
+        """
+            Hook that is invoked on finalizing of the engine procedure.
+        Args:
+            state: See on_start
+
+        Returns:
+
+        """
         ...
 
     def add_meter(self, name: str, meter: Meter):
+        """
+            Add a meter to the engine.
+        Args:
+            name: Name of the meter, served as the key in self.meter_dict.
+            meter: The meter object
+        Returns:
+            None.
+        """
         self.meter_dict[name] = meter
