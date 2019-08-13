-increase the precision and fix the bug that the averaging of patient level score is incorrect -todo: visualization of patient AUC

pycodelib/dlengine/base_engine.py
@@ -23,14 +23,18 @@ class SkinEngine(AbstractEngine):
                  model: nn.Module,
                  loss: nn.Module,
                  iterator_getter: AbstractIteratorBuilder,
-                 val_phases: Sequence[str],
                  patient_col: SheetCollection,
                  sub_class_list: Sequence,
-                 class_partition: Sequence[Union[int, Sequence[int]]]):
+                 class_partition: Sequence[Union[int, Sequence[int]]],
+                 val_phases: Dict[bool, str] = None):
 
         super().__init__(device, model, loss, iterator_getter)
-        self.val_phases: Sequence[str] = val_phases
-
+        if val_phases is None:
+            val_phases = dict({True: "train", False: "val"})
+        self.val_phases: Dict[bool, str] = val_phases
+        assert set(self.val_phases.values()).issubset(AbstractEngine.PHASE_NAMES),\
+            f"Invalid Phase Names{self.val_phases}" \
+            f"Expected from {AbstractEngine.PHASE_NAMES}"
         self.patient_info = patient_col
         self.sub_class_list = sub_class_list
         self.class_partition: np.ndarray = np.asarray(class_partition)
@@ -93,6 +97,8 @@ class SkinEngine(AbstractEngine):
         Returns:
             None
         """
+        if not state['train']:
+            self._evaluation(state)
         torch.cuda.empty_cache()
 
     def on_sample(self, state: Dict[str, Any]):
@@ -142,14 +148,19 @@ class SkinEngine(AbstractEngine):
         Returns:
 
         """
+        self._evaluation(state)
         state['train'] = False
         # todo testing the value() breakpoints
-        self.meter_dict['multi_instance_meter'].value()
+        torch.cuda.empty_cache()
         self.engine.test(self, self.iterator_getter.get_iterator(mode=False, shuffle=False))
 
     def _reset_meters(self):
         for k, v in self.meter_dict.items():
             v.reset()
+
+    def phase_name(self, state):
+        phase = self.val_phases.get(state['train'], None)
+        return phase
 
     def _evaluation(self, state):
         """
@@ -162,14 +173,23 @@ class SkinEngine(AbstractEngine):
 
         """
         # todo test
-        ...
-        print('[Epoch %d] Testing Loss: %.4f (Accuracy: %.2f%%)' % (
-            state['epoch'],
-            self.meter_dict['loss_meter'].value()[0],
-            self.meter_dict['patch_accuracy_meter'].value()[0])
-        )
-        print(self.meter_dict['conf_meter'].value())
-        # todo self._fetch_prediction()
+        phase = self.phase_name(state)
+        if phase is None:
+            #  skip
+            return
+        scores_all_cate, labels_all_cate, row_names, col_names = self.meter_dict['multi_instance_meter'].value()
+
+        loss = self.meter_dict['loss_meter'].value()[0]
+        patch_acc = self.meter_dict['patch_accuracy_meter'].value()[0]
+        patch_auc = self.meter_dict['auc_meter'].value()[0]
+        patch_conf = self.meter_dict['conf_meter'].value()
+        basic_verbose = f"{phase} - [Epoch {state['epoch']}/{self._maxepoch}]. " \
+            f"Loss:= {loss:.5f} " \
+            f"Patch accuracy:= {patch_acc:.2f}  " \
+            f"Patch AUC:= {patch_auc:.2f}"
+        print(basic_verbose)
+        print(patch_conf)
+        breakpoint()
 
     def on_forward(self, state: Dict[str, Any]):
         """
@@ -199,7 +219,6 @@ class SkinEngine(AbstractEngine):
             f"batch_label exceed range {(label.min(), label.max())}. Expected in {self.class_partition}"
 
         # Add Loss Measurements per batch
-        # breakpoint()
         self.meter_dict['loss_meter'].add(loss_scalar)
         # Add accuracy (patch) per batch
         # todo check length of label

pycodelib/dlengine/skeletal.py
@@ -1,7 +1,7 @@
 """
     Abstract classes.
 """
-from typing import Callable, Tuple, Dict, Any, List
+from typing import Callable, Tuple, Dict, Any, List, Set
 import torch
 from torch.utils.data import DataLoader
 from torchnet.engine import Engine
@@ -52,7 +52,7 @@ class AbstractEngine(Callable):
         The abstraction of training using engine. The class is Callable by itself, where its __call__ is defined to
         evaluate the model as serve as the "network/model" parameter of tnt/engine
     """
-
+    PHASE_NAMES: Set[str] = {'train', 'val'}
     @property
     def engine(self) -> Engine:
         """
@@ -168,10 +168,10 @@ class AbstractEngine(Callable):
         """
         debugger.log("process start")
         self._maxepoch = maxepoch
-        self.engine.train(self.__call__, self.iterator_getter.get_iterator(mode=True,
-                                                                           shuffle=True,
-                                                                           batch_size=batch_size,
-                                                                           num_workers=num_workers),
+        self.engine.train(self, self.iterator_getter.get_iterator(mode=True,
+                                                                  shuffle=True,
+                                                                  batch_size=batch_size,
+                                                                  num_workers=num_workers),
                           maxepoch=self._maxepoch, optimizer=optimizer)
 
     @abstractmethod

pycodelib/dlengine/test_dlengine.py
@@ -8,9 +8,8 @@ import glob
 from torchvision import transforms
 from pycodelib.dlengine.iterator_getter import H5DataGetter
 # patient col
-file_list = glob.glob("E:\\melanoma\\melanoma_data\\ROI_new_extractor_20x_v1\\*.png")
+file_list = glob.glob("E:\\melanoma\\melanoma_data\\ROI_new_extractor_v2_20x\\*.png")
 patient_table = SheetCollection(file_list=file_list, sheet_name="E:\\melanoma\\melanoma_data\\Patient_June24.xlsx")
-
 # getter
 pydir_t = 'C:\\pytable\\ROI_inspect\\melanoma_20x_inspect_train.pytable'
 pydir_v = 'C:\\pytable\\ROI_inspect\\melanoma_20x_inspect_val.pytable'
@@ -30,11 +29,11 @@ model = DenseNet(growth_rate=4, block_config=(1, 1), num_init_features=16, bn_si
 device = torch.device('cuda:0')
 loss = nn.CrossEntropyLoss()
 optimizer = Adam(model.parameters())
-engine = SkinEngine(device=device, model=model, loss=loss, iterator_getter=getter, val_phases=['train', 'val'],
+engine = SkinEngine(device=device, model=model, loss=loss, iterator_getter=getter,
                     patient_col=patient_table, sub_class_list=['No Path', 'SCC'],
                     class_partition=[[0], [2, 3]])
 print('start engine')
+
 if __name__ == '__main__':
     engine.process(maxepoch=1, optimizer=optimizer, num_workers=12)
-
 

pycodelib/patients/patient_pred.py
@@ -1,6 +1,6 @@
 from . import PandasRecord
-from .patient_gt import PatientSlideCollection
-from typing import Sequence, List, Dict
+from pycodelib.patients import SheetCollection
+from typing import Sequence, List, Dict, Set
 import numpy as np
 import pandas as pd
 
@@ -8,15 +8,17 @@ import pandas as pd
 class PatientPred(PandasRecord):
     NAME_SCORE: str = "score"
     NAME_PRED: str = "prediction"
+    COL_COUNT: str = "patch_count"
+    NAME_PRESERVED: Set[str] = {NAME_SCORE, NAME_PRED, COL_COUNT}
 
     def __init__(self,
-                 patient_ground_truth: PatientSlideCollection,
+                 patient_ground_truth: SheetCollection,
                  class_list: Sequence[str],
                  ):
         super().__init__()
         self._patient_ground_truth = patient_ground_truth
         self._class_list: np.ndarray = np.asarray(class_list)
-        self.build_df(PatientPred.NAME_SCORE, self._class_list)
+        self.build_df(PatientPred.NAME_SCORE, self._class_list, meta=PatientPred.COL_COUNT)
         self.build_df(PatientPred.NAME_PRED, self._class_list)
 
     @property
@@ -24,15 +26,19 @@ class PatientPred(PandasRecord):
         return self._patient_ground_truth
 
     def load_data(self, table_name, data_list: Sequence, filenames: Sequence[str], flush: bool):
-        PatientSlideCollection.load_data_by_patient(patient_src_record=self.patient_info,
-                                                    target_data_frame=self.get_df(table_name),
-                                                    data_list=data_list,
-                                                    filenames=filenames,
-                                                    flush=flush
-                                                    )
+        SheetCollection.load_data_by_patient(patient_src_record=self.patient_info,
+                                             target_data_frame=self.get_df(table_name), data_list=data_list,
+                                             filenames=filenames, flush=flush)
 
-    def load_score(self, scores_all_category: Sequence[Sequence[float]], filenames: Sequence[str], flush: bool):
-        self.load_data(PatientPred.NAME_SCORE, scores_all_category, filenames, flush)
+    def load_score(self, scores_all_category: Sequence[np.ndarray], filenames: Sequence[str], flush: bool,
+                   expand: bool = False):
+        # add extra column for counts
+        scores_array: np.ndarray = np.atleast_2d(np.asarray(scores_all_category))
+        if expand:
+            scores_all_category_expanded = np.ones((scores_array.shape[0], scores_array.shape[1]+1))
+            scores_all_category_expanded[:, :-1] = scores_array
+            scores_array = scores_all_category_expanded
+        self.load_data(PatientPred.NAME_SCORE, scores_array, filenames, flush)
 
     def entry(self, class_value: str) -> Dict[str, int]:
         assert class_value in self.class_list, f"Undefined Class{class_value} in {self.class_list}"
@@ -56,7 +62,7 @@ class CascadedPred(PatientPred):
         return self._partition
 
     def __init__(self,
-                 patient_ground_truth: PatientSlideCollection,
+                 patient_ground_truth: SheetCollection,
                  class_list: Sequence,
                  partition: Sequence[Sequence[int]],
                  ):
@@ -74,7 +80,7 @@ class CascadedPred(PatientPred):
             # otherwise the sub_table collapses to series and column_sum is reduced to scalar
             patient_ids = [patient_ids]
         sub_table: pd.DataFrame = self.patient_info.patient_ground_truth.loc[patient_ids]
-        # gt class --> current class grouping. At least binary (occurrance). Better to sum the count.
+        # gt class --> current class grouping. At least binary (occurrence). Better to sum the count.
         final_counts: pd.DataFrame = pd.DataFrame()
         for idx, group_idx in enumerate(self._partition):
             group_column: np.ndarray = self.patient_info.class_list[group_idx]